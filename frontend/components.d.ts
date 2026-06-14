2ae261928c5f0bc1e94cbdfca47f54c30e34c797
周兴龙 <12883415+zhouxinglong2001@user.noreply.gitee.com>
2026-06-14 09:48:50 +0800

    feat(v9): stage0 prototype - weekType filter on plan detail

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -41,6 +41,7 @@ declare module 'vue' {
     ElPageHeader: typeof import('element-plus/es')['ElPageHeader']
     ElPagination: typeof import('element-plus/es')['ElPagination']
     ElRadio: typeof import('element-plus/es')['ElRadio']
+    ElRadioButton: typeof import('element-plus/es')['ElRadioButton']
     ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElRow: typeof import('element-plus/es')['ElRow']
     ElSelect: typeof import('element-plus/es')['ElSelect']
